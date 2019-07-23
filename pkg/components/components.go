7f73196d2506591c51be62d62f8de6f00021e946
Kaushal M <kshlmster@gmail.com>
2019-07-23 17:53:25 +0530

    Add package to programatically generate manifests
    The 'components' package allows the generation of the manifests required
    by the CSV, programatically. The package currently also includes
    manifest generation for rook-ceph.
    
    Signed-off-by: Kaushal M <kshlmster@gmail.com>

---
+++ pkg/components/components.go
@@ -0,0 +1,306 @@
+package components
+
+import (
+	ocsv1alpha1 "github.com/openshift/ocs-operator/pkg/apis/ocs/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	extv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	operatorName = "ocs-operator"
+	// TODO: Update to use the correct image
+	image = "quay.io/openshift/ocs-operator:v0.0.1"
+)
+
+// GetDeployment returns a Deployment which deploys the OCS operator
+func GetDeployment() *appsv1.Deployment {
+	deployment := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: operatorName,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"name": operatorName,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"name": operatorName,
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: operatorName,
+					Containers: []corev1.Container{
+						{
+							Name:            operatorName,
+							Image:           image,
+							ImagePullPolicy: corev1.PullPolicy(corev1.PullAlways),
+							// TODO: command being name is artifact of operator-sdk usage
+							Command: []string{operatorName},
+							ReadinessProbe: &corev1.Probe{
+								Handler: corev1.Handler{
+									Exec: &corev1.ExecAction{
+										Command: []string{
+											"stat",
+											"/tmp/operator-sdk-ready",
+										},
+									},
+								},
+								InitialDelaySeconds: 5,
+								PeriodSeconds:       5,
+								FailureThreshold:    1,
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name:  "OPERATOR_NAME",
+									Value: operatorName,
+								},
+								{
+									Name: "POD_NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.name",
+										},
+									},
+								},
+								{
+									Name: "WATCH_NAMESPACE",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.namespace",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return deployment
+}
+
+// GetRole returns the Role required by the OCS operator to function
+func GetRole() *rbacv1.Role {
+	role := &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "rbac.authorization.k8s.io/v1",
+			Kind:       "ClusterRole",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: operatorName,
+			Labels: map[string]string{
+				"name": operatorName,
+			},
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{
+					"",
+				},
+				Resources: []string{
+					"pods",
+					"services",
+					"endpoints",
+					"persistentvolumeclaims",
+					"events",
+					"configmaps",
+					"secrets",
+				},
+				Verbs: []string{
+					"*",
+				},
+			},
+			{
+				APIGroups: []string{
+					""},
+				Resources: []string{
+					"namespaces",
+				},
+				Verbs: []string{
+					"get",
+				},
+			},
+			{
+				APIGroups: []string{
+					"apps",
+				},
+				Resources: []string{
+					"deployments",
+					"daemonsets",
+					"replicasets",
+					"statefulesets",
+				},
+				Verbs: []string{
+					"get",
+					"create",
+				},
+			},
+			{
+				APIGroups: []string{
+					"monitoring.coreos.com",
+				},
+				Resources: []string{
+					"servicemonitors",
+				},
+				Verbs: []string{
+					"get",
+					"create",
+				},
+			},
+			{
+				APIGroups: []string{
+					"ocs.openshift.io",
+				},
+				Resources: []string{
+					"storageclusters",
+					"*",
+				},
+				Verbs: []string{
+					"*",
+				},
+			},
+		},
+	}
+	return role
+}
+
+// GetCRD returns a CustomResourceDefinition for the StorageCluster custom resource
+func GetCRD() *extv1beta1.CustomResourceDefinition {
+	crd := &extv1beta1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apiextensions.k8s.io/v1beta1",
+			Kind:       "CustomResourceDefinition",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "storageclusters.ocs.openshift.io",
+		},
+		Spec: extv1beta1.CustomResourceDefinitionSpec{
+			Group:   "ocs.openshift.io",
+			Version: "v1alpha1",
+			Scope:   "Namespaced",
+
+			Versions: []extv1beta1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1alpha1",
+					Served:  true,
+					Storage: true,
+				},
+			},
+
+			Names: extv1beta1.CustomResourceDefinitionNames{
+				Plural:   "storageclusters",
+				Singular: "storagecluster",
+				Kind:     "StorageCluster",
+			},
+
+			Validation: &extv1beta1.CustomResourceValidation{
+				OpenAPIV3Schema: &extv1beta1.JSONSchemaProps{
+					Properties: map[string]extv1beta1.JSONSchemaProps{
+						"apiVersion": extv1beta1.JSONSchemaProps{
+							Type: "string",
+						},
+						"kind": extv1beta1.JSONSchemaProps{
+							Type: "string",
+						},
+						"metadata": extv1beta1.JSONSchemaProps{
+							Type: "object",
+						},
+						"status": extv1beta1.JSONSchemaProps{
+							Type: "object",
+						},
+						"spec": extv1beta1.JSONSchemaProps{
+							Type: "object",
+							Properties: map[string]extv1beta1.JSONSchemaProps{
+								"instanceType": extv1beta1.JSONSchemaProps{
+									Type: "string",
+								},
+								"manageNodes": extv1beta1.JSONSchemaProps{
+									Type: "boolean",
+								},
+								"storageDeviceSets": extv1beta1.JSONSchemaProps{
+									Type: "array",
+									Items: &extv1beta1.JSONSchemaPropsOrArray{
+										Schema: &extv1beta1.JSONSchemaProps{
+											Properties: map[string]extv1beta1.JSONSchemaProps{
+												"config": extv1beta1.JSONSchemaProps{
+													Type: "object",
+												},
+												"count": extv1beta1.JSONSchemaProps{
+													Type:   "integer",
+													Format: "int64",
+												},
+												"name": extv1beta1.JSONSchemaProps{
+													Type: "string",
+												},
+												"placement": extv1beta1.JSONSchemaProps{
+													Type: "object",
+													Properties: map[string]extv1beta1.JSONSchemaProps{
+														"nodeAffinity": extv1beta1.JSONSchemaProps{
+															Type: "object",
+														},
+														"podAffinity": extv1beta1.JSONSchemaProps{
+															Type: "object",
+														},
+														"podAntiAffinity": extv1beta1.JSONSchemaProps{
+															Type: "object",
+														},
+														"tolerations": extv1beta1.JSONSchemaProps{
+															Type: "array",
+															Items: &extv1beta1.JSONSchemaPropsOrArray{
+																Schema: &extv1beta1.JSONSchemaProps{
+																	Type: "object",
+																},
+															},
+														},
+													},
+												},
+												"resources": extv1beta1.JSONSchemaProps{
+													Type: "object",
+												},
+												"volumeClaimTemplates": extv1beta1.JSONSchemaProps{
+													Type: "object",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return crd
+}
+
+// GetCR returns an example StorageCluster custom resource
+func GetCR() *ocsv1alpha1.StorageCluster {
+	return &ocsv1alpha1.StorageCluster{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "ocs.openshift.io/v1alpha1",
+			Kind:       "StorageCluster",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-storagecluster",
+			Namespace: "openshift-storage",
+		},
+		Spec: ocsv1alpha1.StorageClusterSpec{
+			ManageNodes: false,
+			// TODO: Add an example for StorageDeviceSets
+		},
+	}
+}
---
+++ pkg/components/rook.go
@@ -0,0 +1,813 @@
+package components
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	extv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	rookCephOperator = "rook-ceph-operator"
+	// TODO: Update with correct image
+	rookCephImage = "rook/ceph:master"
+)
+
+// GetRookCephDeployment returns a Deployment that deploys the rook-ceph
+// operator
+func GetRookCephDeployment() *appsv1.Deployment {
+	deployment := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: rookCephOperator,
+			Labels: map[string]string{
+				"operator":        "rook",
+				"storage-backend": "ceph",
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": rookCephOperator,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": rookCephOperator,
+					},
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: "rook-ceph-system",
+					Containers: []corev1.Container{
+						{
+							Name:            rookCephOperator,
+							Image:           rookCephImage,
+							ImagePullPolicy: corev1.PullPolicy(corev1.PullAlways),
+							Args: []string{
+								"ceph",
+								"operator",
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name:  "ROOK_CURRENT_NAMESPACE_ONLY",
+									Value: "true",
+								},
+								{
+									Name:  "FLEXVOLUME_DIR_PATH",
+									Value: "/etc/kubernetes/kublet-plugins/volume/exec",
+								},
+								{
+									Name:  "ROOK_ALLOW_MULTIPLE_FILESYSTEMS",
+									Value: "false",
+								},
+								{
+									Name:  "ROOK_LOG_LEVEL",
+									Value: "INFO",
+								},
+								{
+									Name:  "ROOK_MON_HEALTHCHECK_INTERVAL",
+									Value: "45s",
+								},
+								{
+									Name:  "ROOK_MON_OUT_TIMEOUT",
+									Value: "600s",
+								},
+								{
+									Name:  "ROOK_DISCOVER_DEVICES_INTERVAL",
+									Value: "60m",
+								},
+								{
+									Name:  "ROOK_HOSTPATH_REQUIRES_PRIVILEGED",
+									Value: "true",
+								},
+								{
+									Name:  "ROOK_ENABLE_SELINUX_RELABELING",
+									Value: "true",
+								},
+								{
+									Name:  "ROOK_ENABLE_FSGROUP",
+									Value: "true",
+								},
+								{
+									Name:  "ROOK_DISABLE_DEVICE_HOTPLUG",
+									Value: "false",
+								},
+								{
+									Name: "NODE_NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "spec.nodeName",
+										},
+									},
+								},
+								{
+									Name: "POD_NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.name",
+										},
+									},
+								},
+								{
+									Name: "POD_NAMESPACE",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.namespace",
+										},
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								corev1.VolumeMount{
+									Name:      "rook-config",
+									MountPath: "/var/lib/rook",
+								},
+								corev1.VolumeMount{
+									Name:      "default-config-dir",
+									MountPath: "/etc/ceph",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						corev1.Volume{
+							Name: "rook-config",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+						corev1.Volume{
+							Name: "default-config-dir",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return deployment
+}
+
+// GetRookCephRoles returns a list of Roles needed by the rook-ceph operator
+func GetRookCephRoles() []rbacv1.Role {
+	return []rbacv1.Role{
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "Role",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-system",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+				},
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"pods",
+						"configmaps",
+						"services",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"patch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+				{
+					APIGroups: []string{
+						"apps",
+					},
+					Resources: []string{
+						"daemonsets",
+						"statefulsets",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+			},
+		},
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "Role",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-osd",
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"configmaps",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+			},
+		},
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "Role",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-mgr",
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"pods",
+						"services",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+					},
+				},
+				{
+					APIGroups: []string{
+						"batch",
+					},
+					Resources: []string{
+						"jobs",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+				{
+					APIGroups: []string{
+						"ceph.rook.io",
+					},
+					Resources: []string{
+						"*",
+					},
+					Verbs: []string{
+						"*",
+					},
+				},
+			},
+		},
+	}
+}
+
+// GetRookCephClusterRoles returns a list of ClusterRoles required by the rook-ceph operator
+func GetRookCephClusterRoles() []rbacv1.ClusterRole {
+	return []rbacv1.ClusterRole{
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-cluster-mgmt",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+				},
+			},
+			AggregationRule: &rbacv1.AggregationRule{
+				ClusterRoleSelectors: []metav1.LabelSelector{
+					{
+						MatchLabels: map[string]string{
+							"rbac.ceph.rook.io/aggregate-to-rook-ceph-cluster-mgmt": "true",
+						},
+					},
+				},
+			},
+			Rules: []rbacv1.PolicyRule{},
+		},
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-cluster-mgmt-rules",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+					"rbac.ceph.rook.io/aggregate-to-rook-ceph-cluster-mgmt": "true",
+				},
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"secrets",
+						"pods",
+						"pods/log",
+						"services",
+						"configmaps",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"patch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+				{
+					APIGroups: []string{
+						"apps",
+					},
+					Resources: []string{
+						"deployments",
+						"daemonsets",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+			},
+		},
+
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-global",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+				},
+			},
+			AggregationRule: &rbacv1.AggregationRule{
+				ClusterRoleSelectors: []metav1.LabelSelector{
+					{
+						MatchLabels: map[string]string{
+							"rbac.ceph.rook.io/aggregate-to-rook-ceph-global": "true",
+						},
+					},
+				},
+			},
+			Rules: []rbacv1.PolicyRule{},
+		},
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-global-rules",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+					"rbac.ceph.rook.io/aggregate-to-rook-ceph-global": "true",
+				},
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"pods",
+						"nodes",
+						"nodes/proxy",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+					},
+				},
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"events",
+						"persistentvolumes",
+						"persistentvolumeclaims",
+						"endpoints",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"patch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+				{
+					APIGroups: []string{
+						"storage.k8s.io",
+					},
+					Resources: []string{
+						"storageclasses",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+					},
+				},
+				{
+					APIGroups: []string{
+						"batch",
+					},
+					Resources: []string{
+						"jobs",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+						"create",
+						"update",
+						"delete",
+					},
+				},
+				{
+					APIGroups: []string{
+						"ceph.rook.io",
+					},
+					Resources: []string{
+						"*",
+					},
+					Verbs: []string{
+						"*",
+					},
+				},
+				{
+					APIGroups: []string{
+						"rook.io",
+					},
+					Resources: []string{
+						"*",
+					},
+					Verbs: []string{
+						"*",
+					},
+				},
+			},
+		},
+
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-mgr-cluster",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+				},
+			},
+			AggregationRule: &rbacv1.AggregationRule{
+				ClusterRoleSelectors: []metav1.LabelSelector{
+					{
+						MatchLabels: map[string]string{
+							"rbac.ceph.rook.io/aggregate-to-rook-ceph-mgr-cluster": "true",
+						},
+					},
+				},
+			},
+			Rules: []rbacv1.PolicyRule{},
+		},
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-mgr-cluster-rules",
+				Labels: map[string]string{
+					"operator":        "rook",
+					"storage-backend": "ceph",
+					"rbac.ceph.rook.io/aggregate-to-rook-ceph-mgr-cluster": "true",
+				},
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"configmaps",
+						"nodes",
+						"nodes/proxy",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+					},
+				},
+			},
+		},
+
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-mgr-system",
+			},
+			AggregationRule: &rbacv1.AggregationRule{
+				ClusterRoleSelectors: []metav1.LabelSelector{
+					{
+						MatchLabels: map[string]string{
+							"rbac.ceph.rook.io/aggregate-to-rook-ceph-mgr-system": "true",
+						},
+					},
+				},
+			},
+			Rules: []rbacv1.PolicyRule{},
+		},
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "rbac.authorization.k8s.io/v1",
+				Kind:       "ClusterRole",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "rook-ceph-mgr-system-rules",
+				Labels: map[string]string{
+					"rbac.ceph.rook.io/aggregate-to-rook-ceph-mgr-system": "true",
+				},
+			},
+			Rules: []rbacv1.PolicyRule{
+				{
+					APIGroups: []string{
+						"",
+					},
+					Resources: []string{
+						"configmaps",
+					},
+					Verbs: []string{
+						"get",
+						"list",
+						"watch",
+					},
+				},
+			},
+		},
+	}
+}
+
+// GetRookCephCRDs returns a list of CustomResourceDefinitions for the Rook
+// Ceph custom resources.
+func GetRookCephCRDs() []extv1beta1.CustomResourceDefinition {
+	return []extv1beta1.CustomResourceDefinition{
+		// CephCluster CRD
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "apiextensions.k8s.io/v1beta1",
+				Kind:       "CustomResourceDefinition",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "cephclusters.ocs.openshift.io",
+			},
+			Spec: extv1beta1.CustomResourceDefinitionSpec{
+				Group:   "ceph.rook.io",
+				Version: "v1",
+				Scope:   "Namespaced",
+				Names: extv1beta1.CustomResourceDefinitionNames{
+					Kind:     "CephCluster",
+					ListKind: "CephClusterList",
+					Plural:   "cephclusters",
+					Singular: "cephcluster",
+				},
+
+				Validation: &extv1beta1.CustomResourceValidation{
+					OpenAPIV3Schema: &extv1beta1.JSONSchemaProps{
+						Properties: map[string]extv1beta1.JSONSchemaProps{
+							"spec": extv1beta1.JSONSchemaProps{
+								Type: "object",
+								Properties: map[string]extv1beta1.JSONSchemaProps{
+									"cephVersion": extv1beta1.JSONSchemaProps{
+										Properties: map[string]extv1beta1.JSONSchemaProps{
+											"allowUnsupported": extv1beta1.JSONSchemaProps{
+												Type: "boolean",
+											},
+											"image": extv1beta1.JSONSchemaProps{
+												Type: "string",
+											},
+											"name": extv1beta1.JSONSchemaProps{
+												Type:    "string",
+												Pattern: "^(luminous|mimic|nautilus)$",
+											},
+										},
+									},
+									"dashboard": extv1beta1.JSONSchemaProps{
+										Properties: map[string]extv1beta1.JSONSchemaProps{
+											"enabled": extv1beta1.JSONSchemaProps{
+												Type: "boolean",
+											},
+											"urlPrefix": extv1beta1.JSONSchemaProps{
+												Type: "string",
+											},
+											"port": extv1beta1.JSONSchemaProps{
+												Type: "integer",
+											},
+										},
+									},
+									"dataDirHostPath": extv1beta1.JSONSchemaProps{
+										Type:    "string",
+										Pattern: "^/(\\S+)",
+									},
+									"mon": extv1beta1.JSONSchemaProps{
+										Properties: map[string]extv1beta1.JSONSchemaProps{
+											"allowMultiplePerNode": extv1beta1.JSONSchemaProps{
+												Type: "boolean",
+											},
+											"count": extv1beta1.JSONSchemaProps{
+												Type:    "integer",
+												Maximum: float64ptr(9),
+												Minimum: float64ptr(1),
+											},
+											"preferredCount": extv1beta1.JSONSchemaProps{
+												Type:    "integer",
+												Maximum: float64ptr(9),
+												Minimum: float64ptr(0),
+											},
+										},
+									},
+									"network": extv1beta1.JSONSchemaProps{
+										Properties: map[string]extv1beta1.JSONSchemaProps{
+											"hostNetwork": extv1beta1.JSONSchemaProps{
+												Type: "boolean",
+											},
+										},
+									},
+									"storage": extv1beta1.JSONSchemaProps{
+										Properties: map[string]extv1beta1.JSONSchemaProps{
+											"nodes": extv1beta1.JSONSchemaProps{
+												Type: "array",
+											},
+											"useAllDevices": extv1beta1.JSONSchemaProps{
+												Type: "boolean",
+											},
+											"useAllNodes": extv1beta1.JSONSchemaProps{
+												Type: "boolean",
+											},
+											// TODO: Include StorageClassDeviceSets when merged in
+											// rook-ceph.
+										},
+									},
+								},
+								Required: []string{
+									"mon",
+								},
+							},
+						},
+					},
+				},
+				AdditionalPrinterColumns: []extv1beta1.CustomResourceColumnDefinition{
+					{
+						Name:        "DataDirHostPath",
+						Type:        "string",
+						Description: "Directory user on the K8s nodes",
+						JSONPath:    ".spec.dataDirHostPath",
+					},
+					{
+						Name:        "MonCount",
+						Type:        "string",
+						Description: "Number of MONs",
+						JSONPath:    ".spec.mon.count",
+					},
+					{
+						Name:     "Age",
+						Type:     "date",
+						JSONPath: ".metadata.creationTimestamp",
+					},
+					{
+						Name:        "State",
+						Type:        "string",
+						Description: "Current State",
+						JSONPath:    ".status.state",
+					},
+					{
+						Name:        "Health",
+						Type:        "string",
+						Description: "Ceph Health",
+						JSONPath:    ".status.ceph.health",
+					},
+				},
+			},
+		},
+		// CephBlockPool CRD
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "apiextensions.k8s.io/v1beta1",
+				Kind:       "CustomResourceDefinition",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "cephblockpools.ocs.openshift.io",
+			},
+			Spec: extv1beta1.CustomResourceDefinitionSpec{
+				Group:   "ceph.rook.io",
+				Version: "v1",
+				Scope:   "Namespaced",
+				Names: extv1beta1.CustomResourceDefinitionNames{
+					Kind:     "CephBlockPool",
+					ListKind: "CephBlockPoolList",
+					Plural:   "cephblockpools",
+					Singular: "cephblockpool",
+				},
+			},
+		},
+		// CephObjectStore CRD
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "apiextensions.k8s.io/v1beta1",
+				Kind:       "CustomResourceDefinition",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "cephobjectstores.ocs.openshift.io",
+			},
+			Spec: extv1beta1.CustomResourceDefinitionSpec{
+				Group:   "ceph.rook.io",
+				Version: "v1",
+				Scope:   "Namespaced",
+				Names: extv1beta1.CustomResourceDefinitionNames{
+					Kind:     "CephObjectStore",
+					ListKind: "CephObjectStoreList",
+					Plural:   "cephobjectstores",
+					Singular: "cephobjectstore",
+				},
+			},
+		},
+		// CephObjectStoreUser CRD
+		{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "apiextensions.k8s.io/v1beta1",
+				Kind:       "CustomResourceDefinition",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "cephobjectstoreusers.ocs.openshift.io",
+			},
+			Spec: extv1beta1.CustomResourceDefinitionSpec{
+				Group:   "ceph.rook.io",
+				Version: "v1",
+				Scope:   "Namespaced",
+				Names: extv1beta1.CustomResourceDefinitionNames{
+					Kind:     "CephObjectStoreUser",
+					ListKind: "CephObjectStoreUserList",
+					Plural:   "cephobjectstoreusers",
+					Singular: "cephobjectstoreuser",
+				},
+			},
+		},
+	}
+}
+
+// GetRookCephCRs is not included as we don't intend for users to directly create CephClusters.
