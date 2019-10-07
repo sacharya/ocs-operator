31c51c43cc76dc40a3afa334987cb5ea0f252f10
Sudarshan Acharya <sudarshan.acharya@redhat.com>
2019-10-06 23:31:36 -0500

    Functest for upgrading the operator via olm.

--- functests/setup_teardown.go
+++ functests/setup_teardown.go
@@ -3,6 +3,7 @@ package functests
 import (
 	"flag"
 
+	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	deploymanager "github.com/openshift/ocs-operator/pkg/deploy-manager"
 )
@@ -17,7 +18,8 @@ func BeforeTestSuiteSetup() {
 	err = t.CreateNamespace(TestNamespace)
 	gomega.Expect(err).To(gomega.BeNil())
 
-	err = t.DeployOCSWithOLM(ocsRegistryImage, localStorageRegistryImage)
+	subscriptionChannel := "alpha"
+	err = t.DeployOCSWithOLM(OcsRegistryImage, LocalStorageRegistryImage, subscriptionChannel)
 	gomega.Expect(err).To(gomega.BeNil())
 
 	err = t.WaitForOCSOperator()
@@ -42,4 +44,31 @@ func AfterTestSuiteCleanup() {
 	// Right now uninstall doesn't work. Once uninstall functions
 	// properly, we'll want to uninstall the storage cluster after
 	// the testsuite completes
+}
+
+// UpgradeTestSuiteSetUp is the function called to upgrade the test environment
+func UpgradeTestSuiteSetUp() {
+	flag.Parse()
+
+	if UpgradeToOcsRegistryImage == "" || UpgradeToLocalStorageRegistryImage == "" {
+		ginkgo.Skip("Condition not met for upgrade")
+	}
+
+	t, err := deploymanager.NewDeployManager()
+	gomega.Expect(err).To(gomega.BeNil())
+
+	// Get the current csv before the upgrade
+	csv, err := t.GetCsv()
+	gomega.Expect(err).To(gomega.BeNil())
+
+	subscriptionChannel := "beta"
+	err = t.UpgradeOCSWithOLM(UpgradeToOcsRegistryImage, UpgradeToLocalStorageRegistryImage, subscriptionChannel)
+	gomega.Expect(err).To(gomega.BeNil())
+
+	err = t.WaitForUpgradeOCSOperator(csv.Name, subscriptionChannel)
+	gomega.Expect(err).To(gomega.BeNil())
+
+	// Make sure StorageCluster previously created in the environment is still healthy
+	err = t.WaitOnStorageCluster()
+	gomega.Expect(err).To(gomega.BeNil())
 }
